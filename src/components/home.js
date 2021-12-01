68a2a14ac7c70dfd06cb89a268ff3318b83a1a08
Keegan652 <71681532+Keegan652@users.noreply.github.com>
2021-11-30 23:54:02 -0500

    Update for the board generation
    adding the code for the implementation for the board generation

--- src/components/home.js
+++ src/components/home.js
@@ -40,6 +40,33 @@ function drawCircle() {
   }
 }
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    var x = 63;
+    var y = 75;
+    for (let j = 0; j < 2; j++) {
+      for (let i = 0; i < 19; i++) {
+        y = i * 61 + 30;
+        var w = Math.floor(Math.random() * 17) + 1;
+        x = w * 61;
+        const canvas = this.refs.canvas
+        const ctx = canvas.getContext("2d")
+        const img = this.refs.image
+        ctx.beginPath();
+        ctx.strokeStyle = '#cc0000';
+        ctx.arc(x, y, 25, 0, 2 * Math.PI);
+        ctx.lineWidth = 5;
+        ctx.stroke();
+      }
+    }
+  }
 
   state = {
     email: '',
@@ -65,6 +92,14 @@ export default class Home extends Component {
         })
       }
     });
+     const canvas = this.refs.canvas
+    const ctx = canvas.getContext("2d")
+    const img = this.refs.image
+    img.onload = () => {
+      ctx.drawImage(img, 0, 0)
+      ctx.font = "40px Courier"
+      ctx.fillText(this.props.text, 210, 75)
+    }
   }
   render() {
     return (
@@ -89,12 +124,11 @@ export default class Home extends Component {
             </Dropdown>
           </Nav>
         </Navbar>
-        <form>
-          <input type="button" onClick={<drawCircle />} />
+        <form onClick={this.handleSubmit}>
+          <input type="button" value="Generate climb" />
         </form>
-        <canvas id="myCanvas" width="1080" height="1170"></canvas>
-        {<makeImage />}
-        <img src={require('./kilterboard.png')} />
+        <canvas ref="canvas" width={1080} height={1170} />
+        <img ref="image" src={require('./kilterboard.png')} className="hidden" alt='' />
       </div>
     );
   }
