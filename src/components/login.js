3082a6db7de3f1924cc302c8571d4de4900126ea
kuprovn93 <55200711+kuprovn93@users.noreply.github.com>
2021-12-03 20:02:37 -0500

    Update login.js

--- src/components/login.js
+++ src/components/login.js
@@ -65,49 +65,64 @@ export default class Login extends Component {
   
   render() {
     return (
-<div class="container">
-	<div class="row">
-		<div class="col-lg-8">
-			<div className="col-lg-6 offset-lg-3 card">
-				<h5>Sign In</h5>
-				<img src={DefaultProfilePicture} className="img-thumbnail rounded-circle m-auto" width="100" height="100" alt="User" />
-				<br />
-				<GoogleLogin
-					className="form-control"
-					render={renderProps =>
-				<GoogleButton style={{ width: '100%' }} {...renderProps} />}
-				clientId={REACT_APP_GOOGLE_CLIENT_ID}
-				buttonText="Sign in with Google"
-				onSuccess={this.onGoogleLoginSuccess}
-				onFailure={({ details }) => { toast.error(details) }}
-				/>
-			</div>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-lg-8">
-			<div class="card mb-4">
-				<a href="#!"></a>
-				<div class="card-body">
-					<h2 class="card-title">BoardBot</h2>
-					<h5>About</h5>
-					<p class="card-text">The Kilter board is a modern take on a traditional training wall. Each hold has been scrutinized for comfort and grip style, allowing one to train on open hand holds, incuts, pinches, crimps and slopers. The LED light system will light up the holds that lay out the problem before you. The layout of these holds is standardized across the globe, allowing climbers to set and climb uniform problems no matter where they are on the planet!</p>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-md-4">
-			<div class="card mb-4">
-				<a href="#!"></a>
-				<div class="card-body">
-					<h5>More</h5>
-					<p class="card-text">The program uses Google OAuth Sign In to login into the system. A "Sign In with Google" option shows up to prompt the user to sign in to the site. </p>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
-    );
-  }
-}
+<div class="container" style="padding-top: 2%;">
+      <div class="row">
+        <div class="col-lg-8">
+          <div class="card mb-4">
+            <a href="#!"></a>
+            <div class="card-body">
+                <h2 class="card-title">BoardBot</h2>
+                <h5>About</h5>
+                <p class="card-text">The Kilter board is a modern take on a traditional training wall. Each hold has been scrutinized for comfort and grip style, allowing one to train on open hand holds, incuts, pinches, crimps and slopers. The LED light system will light up the holds that lay out the problem before you. The layout of these holds is standardized across the globe, allowing climbers to set and climb uniform problems no matter where they are on the planet!</p>
+                <a class="btn btn-primary" href="about.html">Learn more →</a>
+            </div>
+          </div>
+        
+      </div>
+      <div class="col-lg-4">
+        <!-- Search widget-->
+        <div class="card mb-4">
+            <div class="card-header">Search</div>
+            <div class="card-body">
+                <div class="input-group">
+                    <input class="form-control" type="text" placeholder="Search..." aria-label="Search..." aria-describedby="button-search" />
+                 <a class="btn btn-primary" href="home.html"> GO →</a>
+                </div>
+            </div>
+        </div>
+        <div id="root"></div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-4">
+          <div class="card mb-4">
+            <a href="#!"></a>
+            <div class="card-body">
+                
+                <h5>More About</h5>
+                <p class="card-text">The program uses Google OAuth Sign In to login into the system. A "Sign In with Google" option shows up to prompt the user to sign in to the site. </p>
+                <a class="btn btn-primary" href="contact.html">Learn more →</a>
+            </div>
+          </div>
+        
+      </div>
+      <div class="col-md-4">
+        <!-- Search widget-->
+        <div class="card mb-4">
+          <a href="#!"></a>
+          <div class="card-body">
+              
+              <h5>Other</h5>
+              <p class="card-text"> After selecting the account, the user data is saved in a local database and displayed and the top right of the navbar.</p>
+              <a class="btn btn-primary" href="other.html">Learn more →</a>
+          </div>
+        </div>
+        <div id="root"></div>
+        </div>
+      </div>
+    </div>
+    <footer class="py-5 bg-light">
+      <div class="container"><p class="m-0 text-center text-black">Copyright &copy; GraduateAlready</p></div>
+  </footer>
+  </body>
+</html>
